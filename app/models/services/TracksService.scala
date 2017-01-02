7906b54860c5f54fd19c954fd11852d748f8e692
lgaleana <lsgaleana@gmail.com>
2017-01-02 16:24:28 -0600

    Implemented method hasTrackBeenRated

--- app/models/services/TracksService.scala
+++ app/models/services/TracksService.scala
@@ -1,21 +1,29 @@
 package models.services
 
 import play.api.db.slick.DatabaseConfigProvider
-import slick.driver.H2Driver.api._
-import slick.lifted.TableQuery
+import slick.driver.PostgresDriver.api._
 import models.schema.Tables._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 class TracksService() {
   
-  def rateAndInsertTrack(spotifyId: String, name: String, stars: Short) = {
-    val db = Database.forConfig("pg-postgres")
-    val tracks = TableQuery[Track]
+  val db = Database.forConfig("pg-postgres")
+  val tracks = TableQuery[Track]
     
+  
+  def rateAndInsertTrack(spotifyId: String, name: String, stars: Short) = {
     val insertAction = DBIO.seq(Track.map(t => (t.spotifyId, t.name, t.stars)) returning Track.map(_.id) += {
       (spotifyId, name, Some(stars))
     })
     
-    db.run(insertAction)
+    try {
+      Await.result(db.run(insertAction), Duration.Inf)
+    } finally db.close
   }
+  
+  def hasTrackBeenRated(spotifyId: String) = db.run(tracks.filter(_.spotifyId === spotifyId).result)
   
 }
